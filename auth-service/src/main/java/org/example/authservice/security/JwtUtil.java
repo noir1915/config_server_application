eb1f65d0a9302b2bd91155f4c2d6e8f26cbc34c7
Dmitrii Berlov <astronaut95@list.ru>
2025-07-11 00:55:14 +0400

    Commit message

--- auth-service/src/main/java/org/example/authservice/security/JwtUtil.java
+++ auth-service/src/main/java/org/example/authservice/security/JwtUtil.java
@@ -9,12 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-import java.util.Base64;
 import java.util.Date;
-
-import static java.security.KeyRep.Type.SECRET;
 
 @Component
 @RequiredArgsConstructor
